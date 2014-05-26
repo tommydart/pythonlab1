a7c7ce3bea8fe8459bc875484b586291e7b06770
Tommis <tommydart@gmail.com>
2014-05-26 11:06:55 +0300

    Patvarkytas kodas pagal PEP8
    Patvarkytas kodas pagal PEP8 standartus.
    Maksimalus eilutės ilgis 79 simboliai
    Ilgų kintamųjų žodžiai atskirti simboliu _

--- stats.py
+++ stats.py
@@ -8,12 +8,13 @@ print 'Folderis:', argv[1]
 folder = argv[1]
 file_out = argv[2]
 class FolderStats:
+
     words = dict()
     letters = dict()
     folder = ''
     files_stats = ''
-    def __init__(self, folderName):
-        self.folder = folderName
+    def __init__(self, folder_name):
+        self.folder = folder_name
 
     def read_stats(self):
         self.folder_stats(folder)
@@ -24,14 +25,18 @@ class FolderStats:
                 self.folder_stats(folder + "\\" + file)
         else:
             file_words, file_letters = self.file_stats(folder)
-            sorted_words = sorted(file_words.iteritems(), key=operator.itemgetter(1), reverse=True)
-            sorted_letters = sorted(file_letters.iteritems(), key=operator.itemgetter(1), reverse=True)
+            sorted_words = sorted(file_words.iteritems(),
+                                  key=operator.itemgetter(1), reverse=True)
+            sorted_letters = sorted(file_letters.iteritems(),
+                                    key=operator.itemgetter(1), reverse=True)
 
             self.files_stats += "\nFailas: "
             self.files_stats += folder
-            self.files_stats += "\nPasikartojantys žodžiai (pagal dažnumą):\n"
+            self.files_stats += "\nPasikartojantys žodžiai " \
+                                "(pagal dažnumą):\n"
             self.files_stats += str(sorted_words)
-            self.files_stats += "\nPasikartojantys simboliai (pagal dažnumą):\n"
+            self.files_stats += "\nPasikartojantys simboliai " \
+                                "(pagal dažnumą):\n"
             self.files_stats += str(sorted_letters)
             for word in file_words:
                 if word not in self.words.keys():
@@ -62,8 +67,10 @@ class FolderStats:
 
 stats = FolderStats(argv[1])
 stats.read_stats()
-sorted_words = sorted(stats.words.iteritems(), key=operator.itemgetter(1), reverse=True)
-sorted_letters = sorted(stats.letters.iteritems(), key=operator.itemgetter(1), reverse=True)
+sorted_words = sorted(stats.words.iteritems(), key=operator.itemgetter(1),
+                      reverse=True)
+sorted_letters = sorted(stats.letters.iteritems(), key=operator.itemgetter(1),
+                        reverse=True)
 
 f = open(file_out, 'w')
 f.write("Pasikartojantys žodžiai (pagal dažnumą):\n")
