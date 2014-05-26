31348251cb880bb9a13d2ef4d1287b525666dbe4
Tommis <tommydart@gmail.com>
2014-05-26 11:00:26 +0300

    Atskirų failų skaičiavimas, koduotė, rezultatas į fail�
    Atskirų failų išvedimas-skaičiavimas. Uždėta koduotė - utf-8. Ir
    pridėtas rezultato įrašymas į failą.

--- stats.py
+++ stats.py
@@ -1,15 +1,17 @@
+ #!/usr/bin/python
+# -*- coding: utf-8 -*-
 from sys import argv
 from os import listdir,path
 import operator
 
 print 'Folderis:', argv[1]
 folder = argv[1]
-
+file_out = argv[2]
 class FolderStats:
     words = dict()
     letters = dict()
     folder = ''
-
+    files_stats = ''
     def __init__(self, folderName):
         self.folder = folderName
 
@@ -22,6 +24,15 @@ class FolderStats:
                 self.folder_stats(folder + "\\" + file)
         else:
             file_words, file_letters = self.file_stats(folder)
+            sorted_words = sorted(file_words.iteritems(), key=operator.itemgetter(1), reverse=True)
+            sorted_letters = sorted(file_letters.iteritems(), key=operator.itemgetter(1), reverse=True)
+
+            self.files_stats += "\nFailas: "
+            self.files_stats += folder
+            self.files_stats += "\nPasikartojantys žodžiai (pagal dažnumą):\n"
+            self.files_stats += str(sorted_words)
+            self.files_stats += "\nPasikartojantys simboliai (pagal dažnumą):\n"
+            self.files_stats += str(sorted_letters)
             for word in file_words:
                 if word not in self.words.keys():
                     self.words[word] = 0
@@ -54,25 +65,14 @@ stats.read_stats()
 sorted_words = sorted(stats.words.iteritems(), key=operator.itemgetter(1), reverse=True)
 sorted_letters = sorted(stats.letters.iteritems(), key=operator.itemgetter(1), reverse=True)
 
-print sorted_words
-print sorted_letters
-#for file in listdir(folder):
- #   if path.isfile(file):
-  #      f = open(file, 'r')
-   #     text = f.read()
-    #    for word in text.split(' '):
-     #       if word not in words.keys():
-      #          words[word] = 0
-       #     words[word] = words[word] + 1
-       # for letter in text:
-       #     if letter not in letters.keys():
-       #         letters[letter] = 0
-       #     letters[letter] = letters[letter] + 1
-       # total += text
-#sorted_words = sorted(words.iteritems(), key=operator.itemgetter(1), reverse=True)
-#sorted_letters = sorted(letters.iteritems(), key=operator.itemgetter(1), reverse=True)
-#print sorted_words
-#print sorted_letters
+f = open(file_out, 'w')
+f.write("Pasikartojantys žodžiai (pagal dažnumą):\n")
+f.write(str(sorted_words))
+f.write("\nPasikartojantys simboliai (pagal dažnumą):\n")
+f.write(str(sorted_letters))
+
+f.write("\nAtskirų failų statistika:")
+f.write(stats.files_stats)
 
 
 
